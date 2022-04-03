e000f2c74e3c1b75fb8939b185970992c9d63149
kostnerek <kostekmc@gmail.com>
2022-04-03 12:25:23 +0200

    added new endpoints

--- app/main.py
+++ app/main.py
@@ -4,6 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({'message': 'hehorki'})
+    return jsonify({'endpoints': {'/': 'Home', '/temperature': 'returns temperature', '/v': 'returns version'}})
 
+@app.route('/temperature')
+def temperature():
+    return jsonify({'temperature': '20'})
+
+@app.route('/v')
+def version():
+    return jsonify({'version': '0.1.0'})
 #app.run()
