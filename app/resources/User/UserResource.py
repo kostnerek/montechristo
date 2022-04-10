8d7c2541842af69f7349c837ee04a1b6f5ab739b
kostnerek <kostekmc@gmail.com>
2022-04-10 18:01:10 +0200

    fixed fckn typo

--- app/resources/User/UserResource.py
+++ app/resources/User/UserResource.py
@@ -2,12 +2,12 @@ from app.models.User.UserModel import UserModel
 from flask_restful import Resource
 
 class User(Resource):
-    def get(self, user_id):
+    def get(self, username):
         user = UserModel.find_by_username(username)
         if not user:
             return {'message': 'User not found'}, 404
         return user.json(), 200
-    def delete(self, user_id):
+    def delete(self, username):
         user = UserModel.find_by_username(username)
         if not user:
             return {'message': 'User not found'}, 404
